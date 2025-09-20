e46ce9ce4b43a3a55ee0c0327f934f9b77dd0cbc
shiva8439 <shivakumar04129@gmail.com>
2025-09-20 19:10:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,14 +15,10 @@ app.use(express.json());
 // MongoDB connection
 
 
-const MONGO_URI = process.env.MONGODB_URI;
 
-if (!MONGO_URI) {
-  console.error("❌ MONGODB_URI not set. Server cannot start.");
-  process.exit(1);
-}
 
-mongoose.connect(MONGO_URI, {
+
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
@@ -386,5 +382,6 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
 
 
