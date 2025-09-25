a18ef9062927873ccc9150b8b1be8561ca202541
shiva8439 <shivakumar04129@gmail.com>
2025-09-25 12:17:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
-const Stop = require('./models/Stop');
+const Stop = require('../models/Stop');
 
 require('dotenv').config();
 
@@ -417,6 +417,7 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
 
 
 
