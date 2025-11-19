9572d98642fa0f6386d82a73cd00558008914128
shiva8439 <shivakumar04129@gmail.com>
2025-11-19 11:58:28 +0530

    Delete routes.js

--- routes.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const routeSchema = new mongoose.Schema({
-  routeName: String,
-  routeNumber: String,
-  stops: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Stop' }]
-});
-
-module.exports = mongoose.model('Route', routeSchema);
